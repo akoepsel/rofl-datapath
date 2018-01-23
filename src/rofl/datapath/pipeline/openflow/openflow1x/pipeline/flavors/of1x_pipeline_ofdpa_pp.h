ede07428585d233370798240b2b6d6b74d423254
Andreas Koepsel <andreas.koepsel@bisdn.de>
2018-01-23 12:41:37 +0100

    OF-DPA pipeline => enforce packet cloning

--- src/rofl/datapath/pipeline/openflow/openflow1x/pipeline/flavors/of1x_pipeline_ofdpa_pp.h
+++ src/rofl/datapath/pipeline/openflow/openflow1x/pipeline/flavors/of1x_pipeline_ofdpa_pp.h
@@ -127,8 +127,11 @@ static inline void __of1x_process_packet_pipeline_ofdpa(const unsigned int tid, 
 
 			ROFL_PIPELINE_INFO("Packet[%p] applying write actions\n",pkt);
 
+			ROFL_PIPELINE_INFO("Packet[%p] before applying write actions, must_replicate=%u\n",pkt, __of1x_process_instructions_must_replicate(&match->inst_grp));
+
 			//Process WRITE actions
-			__of1x_process_write_actions(tid, (of1x_switch_t*)sw, table->number, pkt, __of1x_process_instructions_must_replicate(&match->inst_grp));
+			//__of1x_process_write_actions(tid, (of1x_switch_t*)sw, table->number, pkt, __of1x_process_instructions_must_replicate(&match->inst_grp));
+			__of1x_process_write_actions(tid, (of1x_switch_t*)sw, table->number, pkt, /*enforce packet cloning*/true);
 
 			//Recover the num_of_outputs to release the lock asap
 			num_of_outputs = match->inst_grp.num_of_outputs;
